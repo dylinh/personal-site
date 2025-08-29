a0031e2f55f2b770a58f35ad8d5c26d7eeeacee9
Dylinh Bun <dylinhb@gmail.com>
2025-08-29 01:58:09 -0400

    social links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar"
 export default function Header(){
     return (
         <header>
-            <h1 className="text-animation">DYLINH BUN</h1>
+            <h1 className="text-animation">DYLINH</h1>
             <Navbar />
         </header>
     )
