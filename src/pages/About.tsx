e4f0dc6271e01e3f548f2574dc2c01d497cd6ece
Dylinh Bun <dylinhb@gmail.com>
2025-09-01 21:55:06 -0400

    updates

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -13,7 +13,7 @@ export default function About(){
                         <img src={imgUrl} className="about-img"/>
                     </div>
                     <p className="writing">Hey, my name is Dylinh Bun. I'm a fourth year Computer Science student at Wilfrid Laurier University with experience in both web development and data science. At Loblaws, I automated financial reporting in Python, saving time for a finance team every week.<br/><br/>My main focus now is building modern and responsive websites. I work with HTML, CSS, JavaScript, React, accessibility, and UI design to develop websites that are well designed and performant across devices.</p>
-                    <p className="writing"><br/> In my free time I enjoy swimming, running, writing, and spending time with my family and friends.<br/><br/>I'm currently open to Winter 2026 internships, May 2026 new grad roles and freelance opportunities in website development.</p>
+                    <p className="writing"><br/> In my free time I enjoy swimming, running, writing, and spending time with my family and friends.<br/><br/>I'm currently open to Winter 2026 internships, May 2026 new grad roles and freelance opportunities in website development.<br/><br/>Looking to connect? <a href="mailto:contact@dylinh.com" className="bold" >contact@dylinh.com</a></p>
                     </div>
                     <Footer/>
                 </div>
