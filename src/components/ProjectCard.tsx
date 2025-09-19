8c4214cef53294125d3ff75113de4c5a9e5730ea
Dylinh Bun <dylinhb@gmail.com>
2025-09-18 20:04:12 -0400

    project card stuff

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,9 @@
+export default function ProjectCard(){
+    return (
+        <div>
+            <h2>Personal Website</h2>
+            <p>September 18, 2025</p>
+            <p>A responsive, mobile-first design, and accessible personal website written in TypeScript</p>
+        </div>
+    )
+}
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,10 +1,12 @@
 import BackButton from "../components/BackButton"
+import ProjectCard from "../components/ProjectCard"
 
 export default function Portfolio(){
     return (
         <>
             <BackButton/>
             <p className="about-title text-animation">PORTFOLIO</p>
+            <ProjectCard/>
         </>
     )
 }
