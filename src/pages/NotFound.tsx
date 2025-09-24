74019a0196ea5e319a8134d116e5296a3673494a
Dylinh Bun <dylinhb@gmail.com>
2025-09-24 11:38:30 -0400

    finishing up

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router"
 export default function NotFound(){
     return (
         <div className="fade-in">
-            <h2 className="text-animation error-text">Page does not exist(</h2>
+            <h2 className="text-animation error-text">Page does not exist</h2>
             <div className="home-btn-container">
                 <Link className="home-btn text-animation" to={"/"}>
                 Back to Home
