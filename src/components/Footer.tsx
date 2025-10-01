af5f5be9601e00b0108608d1693033f891a48a1c
Dylinh Bun <dylinhb@gmail.com>
2025-10-01 16:48:24 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { FaLinkedin } from "react-icons/fa";
 import { GrGithub } from "react-icons/gr";
 import { MdMail } from "react-icons/md";
 
@@ -8,6 +9,7 @@ type FooterProps = {
 export default function Footer({className}: FooterProps){
     return (
     <footer className={className} >
+        <a aria-label="Connect with Dylinh on LinkedIn." href="https://linkedin.com/in/dylinh" target="_blank" rel="noopener noreferrer"><FaLinkedin className="icon" size={45}/></a>
         <a href="https://github.com/dylinh" aria-label="Dylinh's GitHub profile containing his projects." target="_blank" rel="noopener noreferrer"><GrGithub className="icon" size={45}/></a>
         <a className="mail-logo" aria-label="Send Dylinh an Email."href="mailto:contact@dylinh.com" target="_blank" rel="noopener noreferrer"><MdMail className="icon" size={45}/></a>
     </footer>
