c7b178eae2f3d3da9b7a799d22d47e00c19fb500
Dylinh Bun <dylinhb@gmail.com>
2026-01-22 14:56:47 -0500

    Email

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -15,7 +15,7 @@ export default function About(){
                             </div>
                             <div className="writing-container">
                                 <p className="writing">I'm a fourth year CS major at Wilfrid Laurier University in Waterloo and interested in all things tech. I built this site after taking a course on front end development! This website is where i'll be showcasing my projects and blog posts related to tech and business.<br/></p>
-                                <p className="writing"><br/>happy to connect: <a href="mailto:contact@dylinh.com" className="bold" >contact@dylinh.com</a></p>
+                                <p className="writing"><br/>Feel free to send me an email: <a href="mailto:contact@dylinh.com" className="bold" >contact@dylinh.com</a></p>
                             </div>
                         </div>
                     </div>
