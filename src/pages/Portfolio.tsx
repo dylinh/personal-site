8f1f9bd5c104f38f0e6c4cfb74958dd3d028f798
Dylinh Bun <dylinhb@gmail.com>
2026-03-09 07:15:41 -0400

    Update Portfolio.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -9,7 +9,7 @@ export default function Portfolio(){
                 <p className="about-title">projects</p>
             </div>
             <div className="blog-card-container">
-                <ProjectCard desc="A responsive, mobile-first, and accessible personal website." date="September 18, 2025" title="dylinh.com" tags={["Typescript", "React"]} path="https://github.com/dylinh/personal-site" />
+                <ProjectCard desc="A responsive, mobile-first, and accessible personal website." date="September 18, 2025" title="dylinh.com" tags={["TypeScript", "React"]} path="https://github.com/dylinh/personal-site" />
                 <ProjectCard desc="An iOS native app to help Laurier students find classmates and meet new people." date="March 8, 2026" title="meet a hawk" tags={["Swift", "Firebase"]} path="https://apps.apple.com/ca/app/meet-a-hawk/id6757995667" />
 
             </div>
