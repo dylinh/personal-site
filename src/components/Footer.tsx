e7f87336913b5619a2072f0d896344be432c4773
Dylinh Bun <dylinhb@gmail.com>
2026-03-20 13:15:34 -0400

    removeemail

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,6 @@ export default function Footer({className}: FooterProps){
     <footer className={className} >
         <a aria-label="Connect with Dylinh on LinkedIn." href="https://linkedin.com/in/dylinh" target="_blank" rel="noopener noreferrer"><FaLinkedin className="icon" size={45}/></a>
         <a href="https://github.com/dylinh" aria-label="Dylinh's GitHub profile containing his projects." target="_blank" rel="noopener noreferrer"><GrGithub className="icon" size={45}/></a>
-        <a className="mail-logo" aria-label="Send Dylinh an Email."href="mailto:contact@dylinh.com" target="_blank" rel="noopener noreferrer"><MdMail className="icon" size={45}/></a>
     </footer>
     )
 }
