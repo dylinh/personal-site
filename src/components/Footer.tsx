8fef16f0c3b65c6181d0e1cacf4d2af83121bc1b
Dylinh Bun <dylinhb@gmail.com>
2026-04-22 22:42:44 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,6 @@ type FooterProps = {
 export default function Footer({className}: FooterProps){
     return (
     <footer className={className} >
-        <a aria-label="Connect with Dylinh on LinkedIn." href="https://linkedin.com/in/dylinh" target="_blank" rel="noopener noreferrer"><FaLinkedin className="icon" size={45}/></a>
         <a href="https://github.com/dylinh" aria-label="Dylinh's GitHub profile containing his projects." target="_blank" rel="noopener noreferrer"><GrGithub className="icon" size={45}/></a>
     </footer>
     )
