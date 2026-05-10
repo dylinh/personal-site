7bb7a0454d268cc34e5534421ba87c505b889678
Dylinh Bun <dylinhb@gmail.com>
2026-05-10 08:57:48 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar(){
     return (
         <nav className="nav-container">
             <Navblock path="/about">about</Navblock>
-            <Navblock path="/portfolio">projects</Navblock>
+            <Navblock path="/projects">projects</Navblock>
             <Navblock path="/blog">blog</Navblock>
         </nav>
     )
