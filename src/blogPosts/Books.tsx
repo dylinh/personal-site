a009fd46cac2aa1b6ee62fd8150c075430266c54
Dylinh Bun <dylinhb@gmail.com>
2026-05-21 20:47:52 -0400

    Update Books.tsx

--- src/blogPosts/Books.tsx
+++ src/blogPosts/Books.tsx
@@ -4,14 +4,13 @@ export default function Books(){
     return (
         <>
         <div className="backbtn-blog-container">
-            <BackButton className="backbtn-blog" path="/books"/>
+            <BackButton className="backbtn-blog" path="/blog"/>
         </div>
         <div className="blog-post-container fade-in">
             <h2 className="blog-post-title">Book Recommendations</h2>
             <h3 className="blog-post-date">May 17, 2026</h3>
             <hr/>
             <ul>
-                <p>Here is my growing list of book recommendations<br/></p>
                 <li>Making a Good Brain Great by Daniel Amen - I can't put into words how important your brain health is and this book describes everything you need to do. </li>
             </ul>
         </div>
